cb094f96343ede7628f58e4ca86935b08140f464
monikhoder <rinmorni@gmail.com>
2024-10-08 09:26:16 +0700

    modify

--- RM/DataModel.Designer.cs
+++ RM/DataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model '\\TRUENAS\Drive Share\Repo\WinForm\RM\RM\DataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Moni\source\repos\WinForm\RM\RM\DataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
